fa71a4a9fce2fd2778f3b5b09fc0f33267b7959d
Greg Jordan-Detamore <greg@gregjd.com>
2016-01-27 13:29:55 -0500

    Implement classes

--- ride-schools/enroll.py
+++ ride-schools/enroll.py
@@ -45,6 +45,87 @@ GRADE_POSITIONS = {
 POSITION_GRADES = dict(reversed(item) for item in GRADE_POSITIONS.items())
 
 
+# Classes
+
+class SchoolOrDistrict():
+
+    def __init__(self, dict_):
+
+        self.code = dict_["schcode"]
+        self.name = dict_["schname"]
+        self.total = dict_["total"]
+        self.enroll = [0]*14  # list with 14 slots, all with value of 0
+
+        given_grades = ["GPK", "GPF", "GKG", "GKF", "G01", "G02", "G03",
+            "G04", "G05", "G06", "G07", "G08", "G09", "G10", "G11", "G12"]
+        for grade in given_grades:
+            new_grade_name = GRADES[grade]
+            list_position = GRADE_POSITIONS[new_grade_name]
+            count = int(dict_[grade])
+            self.enroll[list_position] += count
+
+    def getCode(self):
+
+        return self.code
+
+    def getName(self):
+
+        return self.name
+
+    def getTotal(self):
+
+        return self.total
+
+    def checkEnroll(self, grade_start, grade_end):
+        """Checks a provided grade range against the actual enrollments."""
+
+        if type(grade_start) == int:
+            grade_start = str(grade_start)
+        if type(grade_end) == int:
+            grade_end = str(grade_end)
+        if (type(grade_start) != str) or (type(grade_end) != str):
+            raise TypeError("'grade_start' and 'grade_end' arguments " +
+                "must be of type str.")
+
+        try:
+            start_index = GRADE_POSITIONS[grade_start]
+            end_index = GRADE_POSITIONS[grade_end] + 1
+        except KeyError:
+            raise KeyError("One or both of the given grades is out of range.")
+
+        # Check for grades missing students inside the given grade range:
+        missing = []
+        for i in range(start_index, end_index):
+            if self.enroll[i] == 0:
+                missing.append(POSITION_GRADES[i])
+
+        # Check enrolled students outside the given grade range:
+        extra = []
+        for j in range(0, start_index):
+            if self.enroll[j] != 0:
+                extra.append(POSITION_GRADES[j])
+        for k in range(end_index, len(self.enroll)):
+            if self.enroll[k] != 0:
+                extra.append(POSITION_GRADES[k])
+
+        # Do the enrollments match the grade span provided?
+        match = ((missing == []) and (extra == []))
+
+        return {
+            "match": match,
+            "missing": missing,
+            "extra": extra
+        }
+
+class School(SchoolOrDistrict):
+
+    pass
+
+class District(SchoolOrDistrict):
+
+    pass
+
+
 # Functions
 
 def main(file_name):
@@ -69,84 +150,16 @@ def mapData(raw_data):
     districts = {}
     for d in raw_data:
         schcode = d["schcode"]
-        new_dict = {
-            "code": schcode,
-            "name": d["schname"],
-            "total": d["total"],
-            "enroll": [0]*14  # list with 14 slots, all with value of 0
-        }
-        for grade in given_grades:
-            new_grade_name = GRADES[grade]
-            list_position = GRADE_POSITIONS[new_grade_name]
-            count = d[grade]
-            new_dict["enroll"][list_position] += count
         if len(schcode) == 5:
-            schools[schcode] = new_dict
+            schools[schcode] = School(d)
         elif len(schcode) == 2:
-            districts[schcode] = new_dict
+            districts[schcode] = District(d)
         else:
             raise Exception("Variable 'schcode' can only be 2 or 5 " +
                 "characters long. " + str(len(schcode)) +
                 " characters found in code " + schcode + ".")
 
     return schools, districts
-
-def check(code, grade_start, grade_end, dict_):
-
-    ## add code!
-
-    return {
-        "match": ## bool,
-        "missing": ## list,
-        "extra": ## list
-    }
-
-
-# class School():
-
-#     def __init__(self, dict_):
-
-#         self.code = dict_["schcode"]
-#         self.name = dict_["schname"]
-#         self.total = dict_["total"]
-#         self.enroll = {
-#             "PK": 0,
-#             "K": 0,
-#             "1": 0,
-#             "2": 0,
-#             "3": 0,
-#             "4": 0,
-#             "5": 0,
-#             "6": 0,
-#             "7": 0,
-#             "8": 0,
-#             "9": 0,
-#             "10": 0,
-#             "11": 0,
-#             "12": 0
-#         }
-
-#         for g in GRADES_MAP:
-#             grade = GRADES_MAP[g]
-#             num = dict_[g]
-#             self.enroll[grade] = num
-
-#     def getCode(self):
-
-#         return self.code
-
-#     def getName(self):
-
-#         return self.name
-
-#     def getEnroll(self, gr=None):
-
-#         if gr == None:
-#             return self.total
-#         elif gr in self.enroll:
-#             return self.enroll[gr]
-#         else:
-#             return None
 
 
 if __name__ == "__main__":
