d567889d3b665de93abfad38365c3ac63a2c9553
Greg Jordan-Detamore <greg@gregjd.com>
2016-06-20 15:06:05 -0400

    Add data import methods

--- census/ri-muni/aggregate.py
+++ census/ri-muni/aggregate.py
@@ -4,21 +4,11 @@ import pandas as pd
 def aggregate(data_file, agg_file='catchment_areas.csv',
               on_left='muni_short', on_right='muni', agg_var='area'):
     
-    if (type(data_file) == str) or (type(data_file) == unicode):
-        data_orig = pd.read_csv(data_file)
-    elif type(data_file) == pd.DataFrame:
-        data_orig = data_file
-    else:
-        d
-    try:
-        agg = pd.read_csv(agg_file)
-    except IOError:
-        try:
-            agg = pd.read_csv('../' + agg_file)
-        except IOError:
-            raise IOError(agg_file + ' file not found.')
-    agg_renamed = agg.rename(columns={on_right: on_left})
-    data_joined = data_orig.merge(agg_renamed, how='left', on=on_left)
+    data_orig = _import_data(data_file)
+
+    agg = _get_agg_file(agg_file, on_left, on_right)
+    
+    data_joined = data_orig.merge(agg, how='left', on=on_left)
     data_grouped = data_joined.groupby(agg_var)
 
     data_agg = data_grouped.sum()
@@ -28,3 +18,27 @@ def aggregate(data_file, agg_file='catchment_areas.csv',
         pass
 
     return data_agg
+
+def _import_data(data_file):
+    
+    if (type(data_file) == str) or (type(data_file) == unicode):
+        return pd.read_csv(data_file)
+    elif type(data_file) == pd.DataFrame:
+        return data_file
+    else:
+        raise Exception('data_file must be of type str, unicode, or DataFrame.'
+                        + '\nType found: ' + type(data_file))
+
+def _get_agg_file(agg_file, on_left, on_right):
+
+    try:
+        agg = pd.read_csv(agg_file)
+    except IOError:
+        try:
+            agg = pd.read_csv('../' + agg_file)
+        except IOError:
+            raise IOError(agg_file + ' file not found.')
+    agg_renamed = agg.rename(columns={on_right: on_left})
+
+    return agg_renamed
+
