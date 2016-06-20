10edcafb9d7773b32a989b378c7b1af563c43e42
Greg Jordan-Detamore <greg@gregjd.com>
2016-06-20 14:44:20 -0400

    Add catchment area aggregation

--- census/ri-muni/race/race.py
+++ census/ri-muni/race/race.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append('../')
 
 import cleandata as cd
+import aggregate as agg
 
 
 RACES = {'HD01_VD01': 'total',
@@ -17,12 +18,21 @@ RACES = {'HD01_VD01': 'total',
 RACE_LIST = [RACES[i] for i in sorted(RACES)]
 GEO_COLUMNS = ['geoID_long', 'geoID_short', 'muni_long', 'muni_short']
 
-# Export municipality data
+# Clean municipality data
 data = cd.clean_data('ACS_14_5YR_B03002_with_ann.csv')
 data_new = data[GEO_COLUMNS + sorted(RACES)]
 data_new.rename(columns=RACES, inplace=True)
+
+# Aggregate
+data_agg = agg.aggregate(data_new)
+
+# Calculate percentages
+def calc_pct(row): return row[race]/float(row['total'])
 for race in RACE_LIST:
     if race != 'total':
-def calc_pct(row): return row[race]/float(row['total'])
         data_new['pct_' + race] = data_new.apply(calc_pct, axis=1)
+        data_agg['pct_' + race] = data_agg.apply(calc_pct, axis=1)
+
+# Export to CSV
 data_new.to_csv('race_munis.csv', index=False)
+data_agg.to_csv('race_areas.csv', index=True)
