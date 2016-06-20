a2d3d221468377764961828ff8d94fd40513b6dd
Greg Jordan-Detamore <greg@gregjd.com>
2016-06-20 14:29:45 -0400

    Remove comment

--- census/ri-muni/race/race.py
+++ census/ri-muni/race/race.py
@@ -23,7 +23,6 @@ data_new = data[GEO_COLUMNS + sorted(RACES)]
 data_new.rename(columns=RACES, inplace=True)
 for race in RACE_LIST:
     if race != 'total':
-        # data_new['pct_' + race] = data_new[race]/float(data_new['total'])
         def calc_pct(row): return row[race]/float(row['total'])
         data_new['pct_' + race] = data_new.apply(calc_pct, axis=1)
 data_new.to_csv('race_munis.csv', index=False)
