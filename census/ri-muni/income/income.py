ff074fe2b12faab18eabc4c8d1abf985c940cfc8
Greg Jordan-Detamore <greg@gregjd.com>
2016-07-12 11:30:51 -0400

    Remove old commented code

--- census/ri-muni/income/income.py
+++ census/ri-muni/income/income.py
@@ -10,11 +10,3 @@ data = cd.clean_data('ACS_14_5YR_B19013_with_ann.csv')
 data.drop('HD02_VD01', axis=1, inplace=True)
 data.rename(columns={'HD01_VD01': 'med_hh_inc'}, inplace=True)
 data.to_csv('income_munis.csv', index=False)
-
-### Hold off on the catchment areas - it doesn't make sense to aggregate medians
-##
-### Generate catchment area data
-##areas = pd.read_csv('../catchment_areas.csv', index_col='muni')
-##areas.rename(columns={'muni': 'muni_short'}, inplace=True)
-#### data.join(areas, on='muni_short')
-##data['area'] = data['muni_short'].map(areas['area'])
