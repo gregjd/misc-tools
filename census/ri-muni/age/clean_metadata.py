8e96047183d6731cc8ece0ef6f6eb882daf4de3f
Greg Jordan-Detamore <greg@gregjd.com>
2016-07-12 11:48:18 -0400

    Remove old commented code

--- census/ri-muni/age/clean_metadata.py
+++ census/ri-muni/age/clean_metadata.py
@@ -1,10 +1,6 @@
 import pandas
 
 
-
-# df_filtered = df[df['id'][0:2] != 'HD' and df['desc_split'][0:7] != 'Percent']
-
-
 # Main function
 
 def clean_metadata(file_path='DEC_10_SF1_QTP2_metadata.csv',
@@ -66,14 +62,6 @@ def add_ages(data_frame):
     return data_frame
 
 def filter_out_5yr_groups(data_frame):
-    
-    # return data_frame.loc[data_frame['desc_split'].map(lambda x: len(x)) != 3]
-
-##    old_age_ranges = ['100 to 104 years', '105 to 109 years',
-##                      '110 years and over']
-##
-##    return data_frame.loc[(data_frame['desc_split'].map(lambda x: len(x)) != 3)
-##                          or (data_frame['desc_split'].str[2] != 3)]
 
     return data_frame.loc[data_frame['age'] != 'GROUP5']
 
@@ -91,35 +79,3 @@ def add_sexes(data_frame):
     # data_frame['sex'] = data_frame['desc_split']
 
     return data_frame
-
-##def add_short_codes(data_frame):
-##
-##    data_frame['code'] = data_frame['sex'] + '_' + data_frame['age']
-##    
-##    return data_frame
-
-##def calc_sex(data_frame):
-##
-##    sex_map = {'Both sexes': 'b', 'Male': 'm', 'Female': 'f'}
-##    def split_and_map_sex(text): return sex_map[text.split('; ')]
-##
-##    data_frame['sex'] = 
-##    
-##    return data_frame
-
-##def calc_age():
-##
-##    # 0, 1 ... 99, 100-104, 105-109, 110+
-##    
-##    return
-
-
-
-# Filter out 'Males per 100 females' cases
-# df_f1 = df.loc[df['id'].str[0:2] != 'HD']
-
-# Filter out 'Percent' cases
-# df_f2 = df_f1.loc[df_f1['desc_split'].str[0] != 'Percent']
-
-# Filter out 5-year groupings (leaving only totals and individual years)
-# df_f3 = df_f2.loc[df_f2['desc_split'].map(lambda x: len(x)) != 3]
