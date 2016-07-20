d5d4100e54dd2b624a6e46f89ef7246429195eb4
Greg Jordan-Detamore <greg@gregjd.com>
2016-07-20 14:26:45 -0400

    Move placeholder comment

--- census/ri-muni/process.py
+++ census/ri-muni/process.py
@@ -14,11 +14,11 @@ def process(input_file, output_name, var_map, agg_areas=True):
     data_new = data[GEO_COLUMNS + sorted(var_map.keys())]
     data_new = data_new.rename(columns=var_map)
 
+    # Other stuff: calc, drop
+
     # Aggregate
     if agg_areas:
         data_agg = agg.aggregate(data_new)
-
-    # Other stuff?
 
     # Calculate percentages
     var_list = [var_map[i] for i in sorted(var_map)]
